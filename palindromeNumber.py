8bac3a3f15621fc1ec93c79fdede0fd089c92756
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-08-27 13:01:38 -0700

    working on more leetcode

---
+++ palindromeNumber.py
@@ -0,0 +1,9 @@
+def palNumber(x:int) -> bool:
+    if x < 0:
+        return False
+    return list(str(x)) == list(str(x))[::-1]
+
+
+if __name__ == '__main__':
+    x = 101
+    print(palNumber(x))
