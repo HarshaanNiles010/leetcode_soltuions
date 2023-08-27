c43d116e1504ccdcc4cfe6affa95849fa54c085a
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-08-27 13:38:17 -0700

    longesr Palindrome substring

---
+++ longest_palindrome.py
@@ -0,0 +1,17 @@
+
+def LPS(s:str) -> str:
+    t,ans="",""
+    size=0
+    for i in range(len(s)):
+        for j in range(i,len(s)):
+            t+=s[j]
+            if t==t[::-1] and len(t)>size:
+                ans=t
+                size=len(t)
+        t=""
+    return ans
+
+
+if __name__ == '__main__':
+    s = "babad"
+    print(LPS(s))
