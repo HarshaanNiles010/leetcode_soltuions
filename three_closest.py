6df9ec6e5cdb1b0035bade6ce7c1e3b659ce0ac4
Harshaan Babra <harshaanniles010@gmail.com>
2023-08-29 12:56:25 -0700

    mehhhhhh

---
+++ three_closest.py
@@ -0,0 +1,22 @@
+from typing import List
+def threeClosest(nums: List[int], target: int) -> int:
+    tempSum = 0
+    nums.sort()
+    closest = 9999999999999999999999999999999
+    for i in range(len(nums)):
+        l = i + 1
+        r = len(nums) - 1
+        while l < r:
+            tempSum = nums[i] + nums[l] + nums[r]
+            if tempSum < target:
+                l += 1
+            else:
+                r -= 1
+            if abs(tempSum - target) > abs(closest - target):
+                closest = tempSum
+    return closest
+
+if __name__ == '__main__':
+    nums = [-1,2,1,-4]
+    target = 1
+    print(threeClosest(nums,target))
