9d1d8a77bddce63ceeb71cccaf4d5186676f1561
Harshaan Babra <harshaanniles010@gmail.com>
2023-08-28 19:45:11 -0700

    Update reverse_number.py

--- reverse_number.py
+++ reverse_number.py
@@ -1,4 +1,21 @@
-
+# yeah I hate how I have a perfectly working answer better in readability then this peice of shit here but yeah this is somehow working even when mine wasn't 
+# so I just added my code in the comments
+# I hope it helps
+'''
+def rev(x:int) -> int:
+    MAX_NUM = 2**31
+    if x >= (MAX_NUM - 1) or x <= (-1)*(MAX_NUM):
+        return 0
+    if x < 0:
+        x = x * -1
+        x = list(str(x))[::-1]
+        x = int(''.join(x))
+        x = x * -1
+        return x
+    x = list(str(x))[::-1]
+    x = int(''.join(x))
+    return x
+'''
 def rev(x:int) -> int:
     sign = [1,-1][x < 0]
     x = sign * int(str(abs(x))[::-1])
