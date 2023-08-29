c922d99c2f828a8e589337bd6a1ec76ba8ace2ce
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-08-28 17:41:34 -0700

    working

--- longestsubseq.py
+++ longestsubseq.py
@@ -1,11 +1,15 @@
 from typing import List
 def lengthLCS(s:str) -> int:
-    s_set = set(s)
-    length = 1
-    for i in s:
-        if i not in s_set:
+    hash_set = set()
+    length = 0
+    result_length = 0
+    for i in range(len(s)):
+        while s[i] in hash_set:
+            hash_set.remove(s[length])
             length += 1
-    return length
+        hash_set.add(s[i])
+        result_length = max(result_length,i - length + 1)
+    return result_length
 
 if __name__ == '__main__':
     s = "abcabcbb"
