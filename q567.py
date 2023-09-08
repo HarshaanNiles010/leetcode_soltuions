a948491c2b3df03c87015d3c7c1806efade80cfe
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-09-08 14:48:50 -0700

    solved

--- q567.py
+++ q567.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 """ def getSum(s: str) -> int:
     t = 0
     for i in s:
@@ -20,7 +20,7 @@ def checkInclusion(s1: str, s2: str) -> bool:
             return True
     return False """
 
-def checkInclusion(s1: str, s2: str) -> bool:
+""" def checkInclusion(s1: str, s2: str) -> bool:
     if len(s1) > len(s2):
         return False
     left = 0
@@ -28,7 +28,25 @@ def checkInclusion(s1: str, s2: str) -> bool:
     for i in range(len(s2)):
         if s1 == s2[left + i: right + i]:
             return True
-    return False
+    return False """
+def checkInclusion(s1: str, s2: str) -> bool:
+    s1_map = Counter(s1)
+    match = 0
+        
+    for i in range(len(s2)):
+        # Add right
+        if s2[i] in s1_map:
+            s1_map[ s2[i] ] -= 1
+            if s1_map[ s2[i] ] == 0: match += 1
+            
+            # Subtract left
+        if i+1 > len(s1):
+            if (left:=s2[i-len(s1)]) in s1_map:
+                s1_map[left] += 1
+                # means it came to 1 from 0
+                if s1_map[ left ] == 1: match -= 1    
+    if match == len(s1_map):
+        return True
         
 
 if __name__ == '__main__':
