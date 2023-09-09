75c6c9c5770e416ee4bb86e3a97a8d65eb6f293a
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-09-08 19:17:41 -0700

    done

--- q150.py
+++ q150.py
@@ -7,26 +7,70 @@ def RPN(tokens: List[str]) -> int:
     symbols = ["+","-","*","/"]
     result = []
     s = ""
-    for i in range(len(tokens)):
+    f""" or i in range(len(tokens)):
         if tokens[i] in symbols:
             s = tokens.pop(tokens.index(tokens[i]))
             t_0 = int(tokens.pop(tokens.index(tokens[i - 1])))
-            t_1 = int(tokens.pop(tokens.index(tokens[i - 2])))
+            #t_0 = int(tokens[i - 1])
+            #t_1 = int(tokens.pop(tokens.index(tokens[i - 2])))
+            t_1 = int(tokens[i - 2])
             if s == "+":
                 r = t_0 + t_1
-                tokens.insert(str(r), tokens.index(tokens[i - 2]))
+                tokens[i - 2] = str(r)
+                #tokens.insert(str(r), tokens.index(tokens[i - 2]))
             elif s == "-":
                 r = t_0 - t_1
-                tokens.insert(str(r), tokens.index(tokens[i - 2]))
+                tokens[i - 2] = str(r)
+                #tokens.insert(str(r), tokens.index(tokens[i - 2]))
             elif s == "*":
                 r = t_0 * t_1
-                tokens.insert(str(r), tokens.index(tokens[i - 2]))
+                tokens[i - 2] = str(r)
+                #tokens.insert(str(r), tokens.index(tokens[i - 2]))
             elif s == "/":
                 r = int(t_0/t_1)
-                tokens.insert(str(r), tokens.index(tokens[i - 2]))
-    return int(tokens[0])
+                tokens[i - 2] = str(r)
+                #tokens.insert(str(r), tokens.index(tokens[i - 2])) """
+    """ i = 0
+    while len(tokens) > 1:
+        i += 1
+        if tokens[i] in symbols:
+            s = tokens.pop(tokens.index(tokens[i]))
+            t_0 = int(tokens.pop(tokens.index(tokens[i - 1])))
+            t_1 = int(tokens[i - 2])
+            if s == "+":
+                r = t_0 + t_1
+                tokens[i - 2] = str(r)
+                i = 0
+            elif s == "-":
+                r = t_0 - t_1
+                tokens[i - 2] = str(r)
+                i = 0
+            elif s == "*":
+                r = t_0 * t_1
+                tokens[i - 2] = str(r)
+                i = 0
+            elif s == "/":
+                r = int(t_1/t_0)
+                tokens[i - 2] = str(r)
+                i = 0
+    return int(tokens[0]) """
+    stack = []
+    for c in tokens:
+        if c == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif c == "-":
+            a, b = stack.pop(), stack.pop()
+            stack.append(b - a)
+        elif c == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif c == "/":
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(float(b) / a))
+        else:
+            stack.append(int(c))
+    return stack[0]
 
 if __name__ == '__main__':
-    tokens = ["2","1","+","3","*"]
+    tokens = ["4","13","5","/","+"]
     print(RPN(tokens))
                 
