0f951cd246b5527b1e945462820c268c09a5e8f3
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-09-09 22:57:29 -0700

    not working

--- q20.py
+++ q20.py
@@ -1,7 +1,21 @@
 from typing import List
 
 def isValidPara(s:str) -> bool:
-    pass
+    closer = {
+        ')':'(',
+        '}':'{',
+        '[':']'
+    }
+    stack = []
+    for c in s:
+        if c not in closer:
+            stack.append(c)
+            continue
+        if not stack or stack[-1] != closer[c]:
+            print(False)
+        stack.pop()
+    
+    print(True) 
 
 if __name__ == '__mian__':
     s = "(]"
