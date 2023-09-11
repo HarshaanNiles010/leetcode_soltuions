dd035717bcd8b496caad2313f651ff3ad2b94e0d
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-09-10 21:26:59 -0700

    done

--- q516.py
+++ q516.py
@@ -1,7 +1,17 @@
 from typing import List
 
 def lpsLength(s: str) -> int:
-    pass
+    L = [[None]*(len(s) + 1) for i in range(len(s) + 1)] 
+    for i in range(len(s) + 1 ):
+        for j in range(len(s) + 1 ):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif s[i - 1] == s[::-1][j - 1]:
+                L[i][j] = L[i - 1][j - 1] + 1
+            else:
+                L[i][j] = max(L[i - 1][j], L[i][j - 1])
+    length = L[len(s)][len(s)]
+    return length
 
 
 if __name__ == '__main__':
