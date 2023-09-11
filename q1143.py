c9b51377cf8b9282a9b7cbb135ec0633d003cdf9
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-09-10 21:13:51 -0700

    working lcs length

---
+++ q1143.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def lcsLength(text1: str, text2: str) -> int:
+    pass
+
+if __name__ == '__main__':
+    
+    text1 = "abcde"
+    text2 = "ace" 
+    print(lcsLength(text1, text2))
