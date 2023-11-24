180419264fcf3cb96ac20d7ea8875bee45fa3140
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-23 22:40:50 -0800

    and another down

--- q190.py
+++ q190.py
@@ -6,3 +6,5 @@ def reverseBits(n: int) -> int:
     return new_num
 
 # yeah the people over leetcode decided you shall not take an input so here I am 
+# No but in reality they just decided that the input is an int but the question says it's a string I spent like 35 min on just resolving that 
+# So yeah couldn't care less Anyway fuck it
