b791f3a5b6c002e2d2a5c7a54d44317ae0df73c0
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-27 01:29:21 -0800

    still working

--- q739.py
+++ q739.py
@@ -1,7 +1,18 @@
 from typing import List 
 
-def dailyTemp(temp: List[int]) -> List[int]:
+""" def dailyTemp(temp: List[int]) -> List[int]:
+    res = [0] * len(temp)
+    newStack = []
+    for i, j in enumerate(temp):
+        while newStack and j > newStack[-1][0]:
+            temperature, index = newStack.pop()
+            res[index] = i - index
+        newStack.append((j,i))
+    return res
+"""
+def dailyTemp(temp:List[int]) -> List[int]:
     pass
+
 
 if __name__ == '__main__':
     temperatures = [73,74,75,71,69,72,76,73]
