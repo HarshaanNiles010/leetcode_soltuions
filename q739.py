c5f0e4e26a66c02c4f29b85c8c1bfff16759b337
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-27 01:18:40 -0800

    working on some new questions

---
+++ q739.py
@@ -0,0 +1,8 @@
+from typing import List 
+
+def dailyTemp(temp: List[int]) -> List[int]:
+    pass
+
+if __name__ == '__main__':
+    temperatures = [73,74,75,71,69,72,76,73]
+    print(dailyTemp(temperatures))
