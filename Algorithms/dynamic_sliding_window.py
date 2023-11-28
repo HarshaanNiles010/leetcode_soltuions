29b062658593bde2ff09046c7e9ad747d40912f2
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-28 01:39:15 -0800

    completed the dynamic sliding windows

--- Algorithms/dynamic_sliding_window.py
+++ Algorithms/dynamic_sliding_window.py
@@ -2,7 +2,18 @@
 from typing import List 
 
 def min_length_window(nums: List[int], target:int) -> int:
-    pass
+    min_length = float('inf')
+    leftPtr = 0
+    rightPtr = 0
+    current_sum = 0
+    while rightPtr < len(nums):
+        current_sum += nums[rightPtr]
+        rightPtr += 1
+        while leftPtr < rightPtr and current_sum >= target:
+            current_sum -= nums[leftPtr]
+            leftPtr += 1
+        min_length = min(min_length, rightPtr - leftPtr + 1)
+    return min_length
 
 if __name__ == '__main__':
     nums = [1,2,3,4,5,6]
