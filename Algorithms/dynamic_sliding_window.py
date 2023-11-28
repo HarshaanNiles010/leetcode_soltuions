6b866d32d47c2f348e1598090387c0f02977b803
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-28 01:29:30 -0800

    working on sliding window

---
+++ Algorithms/dynamic_sliding_window.py
@@ -0,0 +1,10 @@
+# The problem is find the  min length window whose sum is greater than or equal to a target
+from typing import List 
+
+def min_length_window(nums: List[int], target:int) -> int:
+    pass
+
+if __name__ == '__main__':
+    nums = [1,2,3,4,5,6]
+    target = 10
+    print(min_length_window(nums, target))
