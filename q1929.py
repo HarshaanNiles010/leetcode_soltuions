829bf830e557583a62d0abea9f784ea2a7940c41
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-29 16:38:54 -0800

    and another one down

--- q1929.py
+++ q1929.py
@@ -3,10 +3,12 @@ from typing import List
 def arrConcat(nums: List[int]) -> List[int]:
     n = len(nums)
     res = [0] * (2* n)
-    for i in range(n):
+    for i in range(2*n):
         res[i] = nums[i%n]
     return res
 
-
+if __name__ == '__main__':
+    nums = [1,2,1]
+    print(arrConcat(nums))
 
     
