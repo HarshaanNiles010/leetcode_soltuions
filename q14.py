0ec0533eb480ac2f424d57e138d3c8d4cd390971
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-01 00:04:10 -0800

    new day new problem

---
+++ q14.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def longestPrefix(strs:[list[str]]) -> str:
+    pass
+
+
+if __name__ == '__main__':
+    strs = ["flower","flow","flight"]
+    print(longestPrefix(strs))
