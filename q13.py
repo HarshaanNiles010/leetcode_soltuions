f76b56040b76a79ef8588981180bc993b1fe7cea
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-11-30 16:10:37 -0800

    your phone linging

--- q13.py
+++ q13.py
@@ -1,5 +1,12 @@
 def romanToInteger(s:str) -> int:
-    pass
+    roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        number=0
+        for i in range(len(s)-1):
+            if roman[s[i]] < roman[s[(i+1)]]:
+                number-=roman[s[i]]
+            else:
+                number+=roman[s[i]]
+        return number+roman[s[-1]]
 
 
 if __name__ == '__main__':
