9f87815c32aed8b50c1da68f6e16090799d70e20
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-01 00:05:45 -0800

    done

--- q14.py
+++ q14.py
@@ -4,7 +4,12 @@ def longestPrefix(strs:[list[str]]) -> str:
     s = ""
     l = len(min(strs, key = len))
     strs.sort()
-    
+    for i in range(l):
+        if strs[0][i] == strs[-1][i]:
+            s += strs[0][i]
+        else:
+            break
+    return s
 
 
 if __name__ == '__main__':
