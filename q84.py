66fb0b1fc0b9be669608d408c9f9f549c808c835
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-05 13:18:32 -0800

    working on a hard problem

---
+++ q84.py
@@ -0,0 +1,9 @@
+from typing import List 
+
+def largestRectangleArea(heights: List[int]) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    heights = [2,1,5,6,2,3]
+    print(largestRectangleArea(heights))
