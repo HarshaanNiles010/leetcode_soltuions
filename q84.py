2c7baf0e549ce4be81ab45dbdf465951c7b172cc
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-05 13:20:52 -0800

    completed

--- q84.py
+++ q84.py
@@ -1,8 +1,18 @@
 from typing import List 
 
 def largestRectangleArea(heights: List[int]) -> int:
-    pass
-
+    maxArea = 0
+    stack = []
+    for i,j in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > j:
+            idx, current_height = stack.pop()
+            maxArea = max(maxArea, current_height * ( i - idx))
+            start = idx
+        stack.append((start,j))
+    for i, j in stack:
+        maxArea = max(maxArea, j * (len(heights) - i))
+    return maxArea
 
 if __name__ == '__main__':
     heights = [2,1,5,6,2,3]
