cefb0afac3b26fbe6ecf32574feac2a6e9fc1263
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-07 00:56:43 -0800

    completed the question

--- q26.py
+++ q26.py
@@ -1,7 +1,12 @@
 from typing import List 
 
 def removeDuplicate(nums:List[int]) -> int:
-    pass
+    r = 1
+    for i in range(1, len(nums)):
+        if nums[i - 1] != nums[i]:
+            nums[r] = nums[i]
+            r += 1
+    return r
 
 if __name__ == '__main__':
     nums = [1,2,2]
