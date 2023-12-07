9d14aabaa6234605c36ecd00a2885c9c9a793986
Harshaan Babra <harshaanniles010@gmail.com>
2023-12-07 01:01:58 -0800

    Update q26.py

--- q26.py
+++ q26.py
@@ -1,5 +1,7 @@
 from typing import List 
-
+# yeah forgot to mention this question sucks ass, the like to dislike ratio is through the roof and the question is poorly worded
+# Развалюха 死ねえ
+# no I am not going to put the translations for these words
 def removeDuplicate(nums:List[int]) -> int:
     r = 1
     for i in range(1, len(nums)):
