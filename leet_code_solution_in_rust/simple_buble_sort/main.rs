f58db6c2a613db7ee9d9333c8ee46541e349bc30
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-21 00:55:23 -0800

    made some changes

--- leet_code_solution_in_rust/simple_buble_sort/main.rs
+++
@@ -1,9 +0,0 @@
-fn main(){
-    let vec_length: i32 = 10;
-    let mut nums: Vec<i32> = vec![0];
-    nums = populate(&mut nums, vec_length);
-}
-
-fn populate(nums: &mut Vec<i32>, length: i32) -> Vec<i32>{
-    
-}
