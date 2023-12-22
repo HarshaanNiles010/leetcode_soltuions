1e93202297fa2c695f6f3d52a96d3da0c894f840
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-22 01:26:44 -0800

    working two pointer solution

---
+++ leet_code_solution_in_rust/sorted_two_pointer/src/main.rs
@@ -0,0 +1,38 @@
+fn main(){
+    let length: i32 = 10;
+    let target: i32 = 14;
+    let mut nums: Vec<i32> = vec![];
+    nums = populate_nums(&mut nums, length);
+    let index_arr: [i32; 2] = sorted_two_pointers(&mut nums, target);
+    println!("The target indexes are: {:?}",index_arr);
+}
+
+fn populate_nums(nums: &mut Vec<i32>, length: i32) -> Vec<i32>{
+    for i in 0..length{
+        nums.push(i + 1);
+    }
+    nums.to_vec()
+}
+
+fn sorted_two_pointers(nums: &mut Vec<i32>, target: i32) -> [i32;2]{
+    let mut left: usize = 0;
+    let mut right: usize = nums.len() - 1;
+    let mut target_sum: i32;
+    let mut target_idx: [i32; 2] = [-1,-1];
+    while left < right{
+        target_sum = nums[left] + nums[right];
+        if target_sum == target{
+            target_idx[0] = left as i32;
+            target_idx[1] = right as i32;
+            return target_idx;
+        }
+        else if target_sum < target{
+            left += 1;
+        }
+        else if target_sum > target{
+            right -= 1;
+        }
+    }
+    return target_idx;
+}
+
