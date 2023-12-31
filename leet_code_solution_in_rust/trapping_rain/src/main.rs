b12513847dfc7be52429dfa83dcb426d28ed78d0
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-12-30 19:32:52 -0800

    got new stuff

--- leet_code_solution_in_rust/trapping_rain/src/main.rs
+++ leet_code_solution_in_rust/trapping_rain/src/main.rs
@@ -1,3 +1,26 @@
+use std::collections::BTreeMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut nums: Vec<i32> = Vec::new();
+    for i in 0..10{
+        nums.push(i + 1);
+    }
+    let freq: BTreeMap<i32, i32> = freq_distro(&mut nums);
+    println!("{:#?}", freq);
+    for _ in 0..nums.len(){
+        println!("{:?}", freq.iter().next_back());
+    }
+}
+
+fn freq_distro(nums: &mut Vec<i32>) -> BTreeMap<i32, i32>{
+    let mut temp: BTreeMap<i32, i32> = BTreeMap::new();
+    for i in 0..nums.len(){
+        if temp.contains_key(&nums[i]) {
+            *temp.get_mut(&nums[i]).unwrap() += 1;
+        }
+        else{
+            temp.insert(nums[i], 1);
+        }
+    }
+    temp
 }
