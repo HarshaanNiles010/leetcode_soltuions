26f8e86252eb9ed4ad81be763a0c50423bb3865d
HarshaanNiles010 <harshaanniles010@gmail.com>
2024-01-23 01:44:57 -0800

    solved subsets2

--- q90.py
+++ q90.py
@@ -2,20 +2,22 @@ from typing import List
 
 def subsetWithDup(nums:List[int]) -> List[List[int]]:
     res = []
-    subset = []
-    i = len(nums) - 1
-    def dfs(i):
-        if i < 0:
-            res.append(subset.copy())
+    nums.sort()
+    def dfs(i, subset):
+        if i == len(nums):
+            res.append(subset[::])
             return
+        # All subsets that include nums[i]
         subset.append(nums[i])
-        dfs(i - 1)
-        nums.pop()
+        dfs(i + 1, subset)
+        subset.pop()
+        # All subsets that don't include nums[i]
         while i + 1 < len(nums) and nums[i] == nums[i + 1]:
             i += 1
-        dfs(i - 1)
-    dfs(i)
-    return res    
+        dfs(i + 1, subset)
+
+    dfs(0, [])
+    return res   
 
 if __name__ == '__main__':
     nums = [1,2,2]
