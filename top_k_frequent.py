d9db5fef0e1d756761108b2ffaceaf51aac6c300
Harshaan Babra <harshaanniles010@gmail.com>
2025-04-03 01:33:40 -0700

    Update top_k_frequent.py

--- top_k_frequent.py
+++ top_k_frequent.py
@@ -13,6 +13,21 @@ def topK(nums:List[int], k:int) -> List[int]:
     for i in range(k):
         rl.append(keys[i])
     return rl
+# This is a faster method, it uses bucket sort to shorten the time it needs to create the required buckets with the count of the various elements
+# then just traverse through the list backwards to get the required result
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for num in nums:
+            count[num] = 1 + count.get(num,0)
+        for num,cnt in count.items():
+            freq[cnt].append(num)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
 
 if __name__ == '__main__':
     nums = [1,1,1,2,2,3]
