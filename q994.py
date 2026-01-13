61fdc116770de74aa5b2b5f22d52773aea634759
HarshaanNiles010 <harshaanniles010@gmail.com>
2026-01-13 15:04:11 +0530

    Changed Some Files

--- q994.py
+++ q994.py
@@ -20,18 +20,18 @@ def calc_time(grid: List[List[int]]) -> int:
     neighbours = [[0,1],[0,-1],[-1,0],[1,0]]
     # run a multiple source BFS to check for time
     while q and fresh > 0:
-        for _ in range(len(q)):
-            i, j = q.popleft()
+        for i in range(len(q)):
+            i, j  = q.popleft()
             for coord_x, coord_y in neighbours:
-                r = i + coord_x
-                c = j + coord_y
-                if (r < 0 or r >= rows or
-                    c < 0 or c >= columns or
+                r = coord_x + i
+                c = coord_y + j
+                if (r < 0 or r == len(grid) or 
+                    c < 0 or c == len(grid[0]) or
                     grid[r][c] != 1):
                     continue
                 # Decrement the fresh oranges if orange is going to rot
                 grid[r][c] = 2
-                q.append([r, c])
+                q.append([r,c])
                 fresh -= 1
         time += 1
     # Return the time it took for oranges to rot otherwise return -1 indicating task is unsuccessful
